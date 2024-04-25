f10d1acf0df80549d1b754519c3e892070db874d
Ariol <ariolkenfack1@gmail.com>
2024-04-25 12:13:24 +0200

    fixed addExpense

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,16 +1,24 @@
 import { useEffect } from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { SignUp } from '../Screen/SignUp';
 import { SignIn } from '../Screen/SignIn';
 import { ForgotPass } from '../Screen/ForgotPass';
 import { DashBoard } from '../Screen/DashBoard';
 import { Home } from '../Screen/Home';
+import { AddExpense } from '../Screen/AddExpense';
 import { initDB } from '../utils/database';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+
+//import { dropTable } from '../utils/database';
+
 /*import { SuppTable } from '../utils/database';*/
 
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function AppNavigation() {
   // Initialize the database
@@ -23,25 +31,49 @@ export default function AppNavigation() {
       console.log(error);
     })
   })
-
+  //gerer la vitesse de transition
+  
   return (
     
-      <Stack.Navigator initialRouteName='SignIn'>
+      <Stack.Navigator initialRouteName='SignIn'
+        
+      >
+        
         <Stack.Screen 
         name='SignIn' 
         options={{
           title: "Login",
+
         }}
         component={SignIn}/>
         <Stack.Screen name='ForgotPassword' component={ForgotPass}/>
         <Stack.Screen name='SignUp' component={SignUp}/>
         <Stack.Screen name='Dash'
-         component={DashBoard}/>
+         component={DashBoard}
+         options={{
+          title: 'DashBoard',
+          headerLeft: null,
+          
+          
+        }}
+         />
         <Stack.Screen name='Home'
         options={{
           title: 'Are you Ready?',
+          headerLeft : null,
+          
+          
+          
         }}
         component={Home}/>
+
+<Stack.Screen name='addExp'
+        options={{
+          title: 'New Expense',
+          headerLeft : null,
+         
+        }}
+        component={AddExpense}/>
       </Stack.Navigator>
 
   );
